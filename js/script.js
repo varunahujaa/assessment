e38739deeb9a7073645d4f5c85c3ffe8c15d47cf
tushar <you@example.com>
2023-03-09 01:26:24 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// let btn = document.getElementsByClassName("faq__button");
+// let hidden = document.getElementsByClassName("hidden");
+
+function myFunction() {
+    let x = document.getElementById("hidden");
+  
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction2() {
+    let x = document.getElementById("hidden2");
+  
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction3() {
+    let x = document.getElementById("hidden3");
+  
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction4() {
+    let x = document.getElementById("hidden4");
+  
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction5() {
+    let x = document.getElementById("hidden5");
+  
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
